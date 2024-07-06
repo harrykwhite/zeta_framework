fee64fd578feb53678ba6dc399650919bde28694
harrykwhite <harrywhiteproductions@gmail.com>
2024-07-06 11:05:05 +1000

    Add more functions for float rectangle types.

--- zfw_common/src/zfw_common_math.c
+++ zfw_common/src/zfw_common_math.c
@@ -48,6 +48,14 @@ void zfw_init_rect(zfw_rect_t *const rect, const int x, const int y, const int w
 	rect->height = height;
 }
 
+void zfw_init_rect_f(zfw_rect_f_t *const rect_f, const float x, const float y, const float width, const float height)
+{
+	rect_f->x = x;
+	rect_f->y = y;
+	rect_f->width = width;
+	rect_f->height = height;
+}
+
 zfw_vec_2d_i_t zfw_get_rect_pos(zfw_rect_t *const rect)
 {
 	return zfw_create_vec_2d_i(rect->x, rect->y);
@@ -58,14 +66,19 @@ zfw_vec_2d_i_t zfw_get_rect_size(zfw_rect_t *const rect)
 	return zfw_create_vec_2d_i(rect->width, rect->height);
 }
 
-int zfw_is_vec_2d_in_rect(const zfw_vec_2d_t vec, const zfw_rect_t *const rect)
+zfw_bool_t zfw_is_vec_2d_in_rect(const zfw_vec_2d_t vec, const zfw_rect_t *const rect)
 {
 	return vec.x >= rect->x && vec.y >= rect->y && vec.x < rect->x + rect->width && vec.y < rect->y + rect->height;
 }
 
-int zfw_do_rects_collide(const zfw_rect_t *const rect_a, const zfw_rect_t *const rect_b)
+zfw_bool_t zfw_do_rects_collide(const zfw_rect_t *const rect_a, const zfw_rect_t *const rect_b)
 {
 	return rect_a->x + rect_a->width >= rect_b->x && rect_a->y + rect_a->height >= rect_b->y && rect_a->x < rect_b->x + rect_b->width && rect_a->y < rect_b->y + rect_b->height;
+}
+
+zfw_bool_t zfw_do_rect_fs_collide(const zfw_rect_f_t *const rect_f_a, const zfw_rect_f_t *const rect_f_b)
+{
+	return rect_f_a->x + rect_f_a->width >= rect_f_b->x && rect_f_a->y + rect_f_a->height >= rect_f_b->y && rect_f_a->x < rect_f_b->x + rect_f_b->width && rect_f_a->y < rect_f_b->y + rect_f_b->height;
 }
 
 void zfw_init_identity_matrix_4x4(zfw_matrix_4x4_t *const mat)
